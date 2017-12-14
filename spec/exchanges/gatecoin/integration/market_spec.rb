54527db55304a72d71d3b154b7f048085ee12d9d
Pablo Fernandez <pfer@me.com>
2017-12-14 14:47:52 +0100

    Add gatecoin order book support

--- spec/exchanges/gatecoin/integration/market_spec.rb
+++ spec/exchanges/gatecoin/integration/market_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe 'Gatecoin integration specs' do
   let(:client) { Cryptoexchange::Client.new }
   let(:btc_hkd_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'HKD', market: 'gatecoin') }
+  let(:btc_eur_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'EUR', market: 'gatecoin') }
 
   it 'fetch pairs' do
     pairs = client.pairs('gatecoin')
@@ -28,5 +29,19 @@ RSpec.describe 'Gatecoin integration specs' do
     expect(ticker.timestamp).to be_a Numeric
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_eur_pair)
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'EUR'
+    expect(order_book.market).to eq 'gatecoin'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to_not be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
   end
 end
