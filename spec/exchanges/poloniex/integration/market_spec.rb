c5d312eb73696867ed3d3b3761b442d63ce0aa2f
Pablo Fernandez <pfer@me.com>
2017-12-15 00:45:54 +0100

    Add poloniex order book support

--- spec/exchanges/poloniex/integration/market_spec.rb
+++ spec/exchanges/poloniex/integration/market_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe 'Poloniex integration specs' do
   let(:client) { Cryptoexchange::Client.new }
   let(:ltc_btc_pair) { Cryptoexchange::Models::MarketPair.new(base: 'LTC', target: 'BTC', market: 'poloniex') }
+  let(:btc_bcn_pair) { Cryptoexchange::Models::MarketPair.new(base: 'BTC', target: 'BCN', market: 'poloniex') }
 
   it 'fetch pairs' do
     pairs = client.pairs('poloniex')
@@ -38,5 +39,19 @@ RSpec.describe 'Poloniex integration specs' do
     expect(ticker.timestamp).to be_a Numeric
     expect(2000..Date.today.year).to include(Time.at(ticker.timestamp).year)
     expect(ticker.payload).to_not be nil
+  end
+
+  it 'fetch order book' do
+    order_book = client.order_book(btc_bcn_pair)
+    expect(order_book.base).to eq 'BTC'
+    expect(order_book.target).to eq 'BCN'
+    expect(order_book.market).to eq 'poloniex'
+    expect(order_book.asks).to_not be_empty
+    expect(order_book.bids).to_not be_empty
+    expect(order_book.asks.first.price).to_not be_nil
+    expect(order_book.bids.first.amount).to_not be_nil
+    expect(order_book.bids.first.timestamp).to_not be_nil
+    expect(order_book.timestamp).to be_a Numeric
+    expect(order_book.payload).to_not be nil
   end
 end
